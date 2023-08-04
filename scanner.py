6da6263e10cc6f418f62760c017cab29a29bc57e
Ava1ls <anslav05@gmail.com>
2023-08-04 22:10:23 +0200

    Modified argument parcing

--- scanner.py
+++ scanner.py
@@ -15,34 +15,42 @@ s_service= ""
 s_os = ""
 
 user_scan_timeout = 1
-user_port_range = 5
+user_port_range_start = 0
+user_port_range_end = 5
 
 start_time = datetime.now()
 
-#Define target Ip or hostname
+#Extracting arguments
 target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4, DNS translation
 
-if len(sys.argv) > 2:
+if len(sys.argv) > 2 and type(sys.argv[2]) == str:
     match sys.argv[2]:
-        case "-s1":
+        case "-sF":
             user_scan_timeout = 0.1
-        case "-s2":
-            user_scan_timeout = 0.2
-        case "-s3":
-            user_scan_timeout = 0.3
-        case "-s4":
-            user_scan_timeout = 0.4
-        case "-s5":
+        case "-sM":
             user_scan_timeout = 0.5
         case _:
-            user_scan_timeout = 1
+            if len(sys.argv) > 2:
+                split = sys.argv[2].split('-s')
+                user_scan_timeout = int(split[1])
+            else:
+                user_scan_timeout = 1
 else:
     user_scan_timeout = 1
 
-if len(sys.argv) > 3:
-    user_port_range = sys.argv[3]
+if len(sys.argv) > 3 or type(sys.argv[2]) != str and sys.argv[3] != "-a":
+    user_port_range_end = sys.argv[3]
+elif sys.argv[3] == "-a":
+    user_port_range_end = 65535
+elif type(sys.argv[3])==str and sys.argv[3] != "-a":
+    range_split = sys.argv[3].split('-')
+    print(range_split)
+    user_port_range_start = int(range_split[0])
+    user_port_range_end = int(range_split[1])
 else:
-    user_port_range = 10
+    user_port_range_start = 0
+    user_port_range_end = 5
+
 #Creating header
 print("-" * 50)
 print("Scanning target " + target)
@@ -51,7 +59,7 @@ print("-" * 50)
 print("PORT         STATE         SERVICE         OS")
 
 try:
-    for port in range(0, int(user_port_range)):
+    for port in range(int(user_port_range_start), int(user_port_range_end)):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(user_scan_timeout)
         result = s.connect_ex((target, port))
@@ -83,6 +91,11 @@ try:
 
 except KeyboardInterrupt:
     print("Ctrl^C Exiting program...")
+    end_time = datetime.now()
+    elapsed_time = end_time - start_time
+    print("-" * 50)
+    print(f"Time elapsed {elapsed_time}, ports scanned - {port_counter}, open ports found - {open_counter}. Terminating program...")
+    print("-" * 50)
     sys.exit()
 
 except socket.gaierror: 
